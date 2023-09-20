2e678b6934720cc6ff9eeb5f88462f9a8ac1f2ca
Kento182 <kento.182@hotmail.com>
2023-09-20 16:26:06 -0600

    Usar postgres desde Docker

--- app/settings.py
+++ app/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "dj_ctl_comb",
+        "USER": "postgres",
+        "PASSWORD": "123456",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
