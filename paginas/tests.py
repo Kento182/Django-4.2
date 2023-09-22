1492b49a75859deebcc6f889e3493e50cde93d6a
Kento182 <kento.182@hotmail.com>
2023-09-22 16:38:38 -0600

    pruebas de la pagina about

--- paginas/tests.py
+++ paginas/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class AboutPageTests(SimpleTestCase):
+    def test_existe_ruta(self):
+        response = self.client.get("/pages/about")
+        self.assertEqual(response.status_code, 200)
+        
+    def test_nombre_ruta_about(self):
+        response = self.client.get(reverse("pages:about"))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_plantilla_contiene_html(self):
+        response = self.client.get(reverse("pages:about"))
+        self.assertContains(response, "Aplicacion Elaborada para el Curso de Django para Profecionales")
+        
+    def test_plantilla_no_contiene_html(self):
+        response = self.client.get(reverse("pages:about"))
+        self.assertNotContains(response, "KENTO")
