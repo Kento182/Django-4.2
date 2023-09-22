4dfeedd16df5dd767fc8a0a3d9eddadab8a7c652
Kento182 <kento.182@hotmail.com>
2023-09-21 19:15:55 -0600

    test para crear usuarios y superusuarios

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class CustomUserTest(TestCase):
+    def test_crear_usuario(self):
+        Usr = get_user_model()
+        usr = Usr.objects.create_user(
+            username = "kento",
+            email = "kento@gmail.com",
+            password = "123"
+        )
+        
+        #Que esperamos
+        self.assertEqual(usr.username,"kento")
+        self.assertEqual(usr.email,"kento@gmail.com")
+        self.assertTrue(usr.is_active)
+        self.assertFalse(usr.is_staff)
+        self.assertFalse(usr.is_superuser)
+        
+    def test_crear_superusuario(self):
+        Usr = get_user_model()
+        usr = Usr.objects.create_superuser(
+            username = "kento",
+            email = "kento@gmail.com",
+            password = "123"
+        )
+        
+        #Que esperamos
+        self.assertEqual(usr.username,"kento")
+        self.assertEqual(usr.email,"kento@gmail.com")
+        self.assertTrue(usr.is_active)
+        self.assertTrue(usr.is_staff)
+        self.assertTrue(usr.is_superuser)
