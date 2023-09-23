2b08e9eac9a86fdbac9470b26e733c5e9b1fa0ee
Kento182 <kento.182@hotmail.com>
2023-09-22 19:47:05 -0600

    plantillas y aplicación del include

--- paginas/tests.py
+++ paginas/tests.py
@@ -3,18 +3,29 @@ from django.urls import reverse
 
 
 class AboutPageTests(SimpleTestCase):
-    def test_existe_ruta(self):
-        response = self.client.get("/pages/about")
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        self.response = self.client.get("/pages/about")
+    
+    def test_url_about(self):
+        self.assertEqual(self.response.status_code, 200)
         
-    def test_nombre_ruta_about(self):
-        response = self.client.get(reverse("pages:about"))
-        self.assertEqual(response.status_code, 200)
+    def test_html_about(self):
+        self.assertTemplateUsed(self.response, "paginas/about.html")
+        self.assertContains(self.response, "Aplicacion Elaborada para el Curso de Django para Profecionales")
+        self.assertNotContains(self.response, "KENTO")
+    
+    # def test_existe_ruta(self):
+    #     response = self.client.get("/pages/about")
+    #     self.assertEqual(response.status_code, 200)
         
-    def test_plantilla_contiene_html(self):
-        response = self.client.get(reverse("pages:about"))
-        self.assertContains(response, "Aplicacion Elaborada para el Curso de Django para Profecionales")
+    # def test_nombre_ruta_about(self):
+    #     response = self.client.get(reverse("pages:about"))
+    #     self.assertEqual(response.status_code, 200)
         
-    def test_plantilla_no_contiene_html(self):
-        response = self.client.get(reverse("pages:about"))
-        self.assertNotContains(response, "KENTO")
+    # def test_plantilla_contiene_html(self):
+    #     response = self.client.get(reverse("pages:about"))
+    #     self.assertContains(response, "Aplicacion Elaborada para el Curso de Django para Profecionales")
+        
+    # def test_plantilla_no_contiene_html(self):
+    #     response = self.client.get(reverse("pages:about"))
+    #     self.assertNotContains(response, "KENTO")
