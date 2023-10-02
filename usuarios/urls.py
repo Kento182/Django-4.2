397f353521d8283f908951176149bd220c403b44
Kento182 <kento.182@hotmail.com>
2023-10-02 10:31:35 -0600

    Usuarios - Registro de usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,4 +8,5 @@ app_name="usuarios"
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('login2/', auth_views.LoginView.as_view(template_name="usuarios/login2.html"), name="login2"),
+    path('registro/', RegistroUsuarioView.as_view(), name="registro"),
 ]
