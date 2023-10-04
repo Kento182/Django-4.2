f00c22912a6007762d5a74b4d47bb76598fd556d
Kento182 <kento.182@hotmail.com>
2023-10-04 12:36:01 -0600

    Ctrl_comb - se crea modelo marca

--- ctrl_comb/models.py
+++ ctrl_comb/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Mark(models.Model):
+    decript = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.decript
+    
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
