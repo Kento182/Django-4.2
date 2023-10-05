b4c0deccc34f474d66685f03400ad8a8018dffc3
Kento182 <kento.182@hotmail.com>
2023-10-04 18:59:36 -0600

    Ctrl_comb - eliminar marca

--- ctrl_comb/urls.py
+++ ctrl_comb/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
     path('mark/', MarkList.as_view(), name="mark_list"),
     path('mark/save', mark_save, name="mark_save"),
+    path('mark/delete/<int:pk>', mark_delete, name="mark_delete"),
 ]
--- ctrl_comb/views.py
+++ ctrl_comb/views.py
@@ -24,3 +24,16 @@ def mark_save(request):
     context["obj"] = obj
     
     return render(request, template_name, context)
+
+
+def mark_delete(request, pk):
+    context = {}
+    template_name = "ctrl_comb/mark-list.html"
+    
+    o = Mark.objects.filter(id=pk).first()
+    o.delete()  # type: ignore
+    
+    obj = Mark.objects.all().order_by("decript")
+    context["obj"] = obj
+    
+    return render(request, template_name, context)
