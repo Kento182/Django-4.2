8997acd23a4b6fa9786ea6a40cda407dfe748257
Kento182 <kento.182@hotmail.com>
2023-10-05 18:39:04 -0600

    Ctrl_comb - Editar marca con htmx

--- ctrl_comb/urls.py
+++ ctrl_comb/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('mark/', MarkList.as_view(), name="mark_list"),
     path('mark/save', mark_save, name="mark_save"),
     path('mark/delete/<int:pk>', mark_delete, name="mark_delete"),
+    path('mark/edit/<int:pk>', mark_edit, name="mark_edit"),
 ]
--- ctrl_comb/views.py
+++ ctrl_comb/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 from .models import *
+from .forms import *
 
 
 class MarkList(ListView):
@@ -18,8 +19,19 @@ def mark_save(request):
     if request.method == "POST":
         i = request.POST.get("id")
         d = request.POST.get("decript")
+        o = None
         
-        o = Mark.objects.create(decript = d)
+        if i:
+            o = Mark.objects.filter(id=i).first()
+        else:
+            o = Mark.objects.filter(decript = d)
+            
+        if o:
+            o.decript = d   # type: ignore
+            o.save()    # type: ignore
+        else:                 
+            o = Mark.objects.create(decript = d)
+            
     obj = Mark.objects.all().order_by("decript")
     context["obj"] = obj
     
@@ -36,4 +48,19 @@ def mark_delete(request, pk):
     obj = Mark.objects.all().order_by("decript")
     context["obj"] = obj
     
+    return render(request, template_name, context)
+
+
+def mark_edit(request, pk=None):
+    context = {}
+    template_name = "ctrl_comb/mark-frm.html"
+    
+    if pk:
+        o = Mark.objects.filter(id=pk).first()
+        frm = MarkForm(instance=o)
+    else:
+        frm = MarkForm()
+        
+    context["form"] = frm
+    context["obj"] = o  # type: ignore
     return render(request, template_name, context)
