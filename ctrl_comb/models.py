a40f792b3fdc0aacd8c425ac6903069457a505b6
Kento182 <kento.182@hotmail.com>
2023-10-05 19:31:37 -0600

    Ctrl_comb - Modelo con ForeignKey

--- ctrl_comb/models.py
+++ ctrl_comb/models.py
@@ -10,3 +10,16 @@ class Mark(models.Model):
     class Meta:
         verbose_name = "Marca"
         verbose_name_plural = "Marcas"
+        
+
+class Modelo(models.Model):
+    descript = models.CharField(max_length=50, db_comment="Descripción del Modelo del Vehículo")    #type: ignore
+    mark = models.ForeignKey(Mark, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return f"{self.mark} - {self.descript}"
+    
+    class Meta:
+        verbose_name = "Modelo"
+        verbose_name_plural = "Modelos"
+        db_table_comment = "Modelos de Vehiculos"
